c37d41319a9ac0773eb72ebd6d5c15836c4962bb
Bea Luise <uson.bealouise@student.auf.edu.ph>
2026-08-19 06:41:29 +0800

    add audio upload input and display

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -1,22 +1,36 @@
 <?php
-
 $upload_directory = getcwd() . '/uploads/';
 $relative_path = '/uploads/';
 
 // Handle Text File
-$uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
-$temporary_file = $_FILES['text_file']['tmp_name'];
-
-if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
-    $text_file_content = file_get_contents($uploaded_text_file, 'r');
-    ?>
-    <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
-    <?php
-} else {
-    echo 'Failed to upload file';
+if (!empty($_FILES['text_file']['name'])) {
+    $uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
+    $temporary_file = $_FILES['text_file']['tmp_name'];
+    if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
+        $text_file_content = file_get_contents($uploaded_text_file);
+        ?>
+        <h3>Text File</h3>
+        <textarea cols="70" rows="30"><?php echo htmlspecialchars($text_file_content); ?></textarea>
+        <?php
+    } else {
+        echo 'Failed to upload text file';
+    }
 }
 
-
-echo '<pre>';
-var_dump($_FILES);
-exit;
+// Handle Audio File
+if (!empty($_FILES['audio_file']['name'])) {
+    $uploaded_audio_file = $upload_directory . basename($_FILES['audio_file']['name']);
+    $temporary_audio = $_FILES['audio_file']['tmp_name'];
+    if (move_uploaded_file($temporary_audio, $uploaded_audio_file)) {
+        $audio_relative = $relative_path . basename($_FILES['audio_file']['name']);
+        ?>
+        <h3>Audio File</h3>
+        <audio controls>
+            <source src="<?php echo htmlspecialchars($audio_relative); ?>" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+        <?php
+    } else {
+        echo 'Failed to upload audio file';
+    }
+}
